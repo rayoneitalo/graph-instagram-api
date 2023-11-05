2d973ee67a3e9016f08bdf50fd496c0cf42519d3
rayoneitalo <rayoneitalo@gmail.com>
2023-11-05 02:38:04 +0000

    fix(auth): exchange token

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -9,9 +10,19 @@ app.listen(PORT, () => {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile('./index.html')
+    res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/auth', (req, res) => {
-    res.send('You are authenticated!')
+    fetch('https://api.instagram.com/oauth/authorize?client_id=644528484504310&redirect_uri=https://graph-instagram-api.vercel.app/&scope=user_profile,user_media&response_type=code', {
+        method: 'GET'
+    })
+        .then(response => console.log(response))
+        .then(data => {
+            // Manipular a resposta do servidor, se necessário
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Erro:', error);
+        });
 })
