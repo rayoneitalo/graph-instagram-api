400c2ac58e8f2f1ce8cdfcd630737e99b9552cd5
rayoneitalo <rayoneitalo@gmail.com>
2023-11-05 02:56:43 +0000

    fix(auth): fix fetch uri

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ app.get('/', (req, res) => {
 })
 
 app.get('/auth', (req, res) => {
-    fetch('https://api.instagram.com/oauth/authorize?client_id=644528484504310&redirect_uri=https://graph-instagram-api.vercel.app/&scope=user_profile,user_media&response_type=code', {
-        method: 'GET'
-    })
-        .then(response => console.log(response))
-        .then(data => {
-            // Manipular a resposta do servidor, se necessário
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
+    // fetch('https://api.instagram.com/oauth/authorize?client_id=644528484504310&redirect_uri=https://graph-instagram-api.vercel.app/auth&scope=user_profile,user_media&response_type=code', {
+    //     method: 'GET',
+
+    // })
+    //     .then(response => console.log(response))
+    //     .then(data => {
+    //         // Manipular a resposta do servidor, se necessário
+    //         console.log(data);
+    //     })
+    //     .catch(error => {
+    //         console.error('Erro:', error);
+    //     });
 })
