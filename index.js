a4c724c8654f219f220b78f2b20df093842c158d
rayoneitalo <rayoneitalo@gmail.com>
2023-11-05 15:48:11 +0000

    feat: send access token to view

--- index.js
+++ index.js
@@ -14,16 +14,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/auth', (req, res) => {
-    // fetch('https://api.instagram.com/oauth/authorize?client_id=644528484504310&redirect_uri=https://graph-instagram-api.vercel.app/auth&scope=user_profile,user_media&response_type=code', {
-    //     method: 'GET',
+    const code = req.query.code;
 
-    // })
-    //     .then(response => console.log(response))
-    //     .then(data => {
-    //         // Manipular a resposta do servidor, se necessário
-    //         console.log(data);
-    //     })
-    //     .catch(error => {
-    //         console.error('Erro:', error);
-    //     });
+    res.send({
+        message: "Authenticated!",
+        code: code
+    })
+
 })
