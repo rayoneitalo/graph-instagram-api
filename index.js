8c8838557abb51705eba344eacdfd2fa9378e2fb
rayoneitalo <rayoneitalo@gmail.com>
2023-11-12 02:44:18 -0300

    feat: change generate url access token

--- index.js
+++ index.js
@@ -16,19 +16,6 @@ app.get('/', (req, res) => {
 app.get('/auth', (req, res) => {
     const code = req.query.code;
 
-    fetch('https://api.instagram.com/oauth/authorize?client_id=715636090427857&redirect_uri=https://graph-instagram-api.vercel.app/auth&scope=user_profile&response_type=code&state=1', {
-        method: 'GET',
-        mode: 'no-cors'
-    })
-        .then(response => response.json())
-        .then(data => {
-            // Manipular a resposta do servidor, se necessário
-            console.log(data);
-        })
-        .catch(error => {
-            console.error('Erro:', error);
-        });
-
     res.send({
         message: "Authenticated!",
         code: code
