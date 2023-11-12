ac27acf827a528455610e663ff932f8982548a73
rayoneitalo <rayoneitalo@gmail.com>
2023-11-12 06:13:14 +0000

    fix: change request url

--- index.js
+++ index.js
@@ -16,9 +16,18 @@ app.get('/', (req, res) => {
 app.get('/auth', (req, res) => {
     const code = req.query.code;
 
-    res.send({
-        message: "Authenticated!",
-        code: code
-    })
 
+    fetch(`https://api.instagram.com/oauth/access_token?client_id=644528484504310&client_secret=f04612803c60e8d219ebc4664df0bd85&code=${code}&grant_type=authorization_code&redirect_uri=https://graph-instagram-api.vercel.app/auth`, {
+        method: 'POST',
+        mode: 'no-cors'
+    })
+        .then((response) => console.log(response))
+        .then(() => {
+            res.send({
+                message: "Authenticated!",
+                code: code
+            })
+        }).catch(error => {
+            console.log('Erro: ', error);
+        })
 })
